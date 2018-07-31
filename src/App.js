9ce2f48504e7e566cd27b19d3416f1f2730f1941
Mariya <mariyawhitcomb@gmail.com>
2018-07-31 19:29:22 -0400

    set up common components

--- src/App.js
+++ src/App.js
@@ -1,47 +1,21 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
-import { createStackNavigator } from "react-navigation";
-import Home from "./components/Home";
-import List from "./components/List";
-import Form from "./components/Form";
+import { Loading } from "./components/common/";
+import Auth from "./screens/Auth";
+import LoggedIn from "./screens/LoggedIn";
 
-export default createStackNavigator({
-  Home: {
-    screen: Home,
-    navigationOptions: {
-      title: "Home",
-      headerBackTitle: "Back"
-    }
-  },
-  List: {
-    screen: List,
-    navigationOptions: {
-      title: "List"
-    }
-  },
-  Form: {
-    screen: Form,
-    navigationOptions: {
-      title: "Form"
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      jwt: ""
+    };
+  }
+
+  render() {
+    if (!this.state.jwt) {
+      return <Auth />;
+    } else if (this.state.jwt) {
+      return <LoggedIn />;
     }
   }
-});
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
-});
+}
